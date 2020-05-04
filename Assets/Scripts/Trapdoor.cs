c16e6713a61045512fc812d97db42a0527a3093e
Necr0o0 <radekklaser@gmail.com>
2020-05-05 00:58:36 +0200

    TrapDoor on main level

---
+++ Assets/Scripts/Trapdoor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Trapdoor : MonoBehaviour
+{
+    public Rigidbody trapDoorLeft;
+    public Rigidbody trapDoorRight;
+    
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OpenDoor();
+        }
+    }
+
+    //method to open the trapdoor
+    void OpenDoor()
+    {
+        trapDoorLeft.isKinematic = false;
+        trapDoorRight.isKinematic = false;
+    }
+    
+}
