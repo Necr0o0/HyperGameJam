1a90c7d9255db0c47a2c2e8fb0da0d5eb14719de
Toster <tostermocy@gmail.com>
2020-05-07 19:58:07 +0200

    palletes

---
+++ Assets/Scripts/Palette.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Palette", menuName = "Palette")]
+public class Palette : ScriptableObject
+{
+    public Color boxColor;
+    public Color funnelColor;
+    public Color backgroundColor;
+    public Color groundColor;
+    public Color ball1Color;
+    public Color ball2Color;
+
+}
