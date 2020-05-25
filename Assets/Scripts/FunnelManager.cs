8a7374d8d4f241b7ac14aae314fcbf63d67d907e
Necr0o0 <radekklaser@gmail.com>
2020-05-25 03:06:19 +0200

    improvements

--- Assets/Scripts/FunnelManager.cs
+++ Assets/Scripts/FunnelManager.cs
@@ -9,12 +9,15 @@ public class FunnelManager : MonoBehaviour
     public Renderer Funnel;
     public TextMeshPro ballsText;
     private float originalSize;
+    private Vector3 scale;
     public float counterInsideBox;
     // Start is called before the first frame update
     void Start()
     {
         originalSize = ballsText.fontSize;
         ballsText.text = "0";
+        scale = transform.localScale;
+
 
     }
 
@@ -26,6 +29,11 @@ public class FunnelManager : MonoBehaviour
         var sequence = DOTween.Sequence();
         sequence.Append(DOTween.To(() => ballsText.fontSize, x => ballsText.fontSize = x, ballsText.fontSize + 1.5f, 0.1f));
         sequence.Join(DOTween.To(() => ballsText.fontSize, x => ballsText.fontSize = x, originalSize, 1f));
+        
+        var sequence2 = DOTween.Sequence();
+        sequence2.Append(transform.DOScaleY(5.4f,0.5f));
+        sequence2.Join(transform.DOScaleY(scale.y,2f));
+
         
         Funnel.material.color = Color.Lerp(   Funnel.material.color, new Color(color.r,color.g,color.b,0), counterInsideBox / 2000f);
     }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -16,21 +16,21 @@ public class movement : MonoBehaviour
 
     void Update()
     {
-        //MoveLeftAndRight();
+        MoveLeftAndRight();
         Rotate();
     }
 
     private void MoveLeftAndRight()
     {
         pos = transform.localPosition;
-        pos.x = Mathf.Sin(Time.time+seed) - 1.834354f;
+        pos.x = Mathf.Sin(Time.time + seed) - 2.0f;
         transform.localPosition = pos;
     }
 
     private void Rotate()
     {
         var angle = transform.localRotation.eulerAngles;
-        angle.z = Mathf.Sin(Time.time + seed)*10f - 1.834354f;
+        angle.z = Mathf.Sin(Time.time + seed) * 10f - 2.0f;
         transform.localRotation = Quaternion.Euler(angle);
     }
 }
